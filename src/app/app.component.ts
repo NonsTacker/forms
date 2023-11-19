e77cd6ec1b64cf153c29261c0cef120c435c608f
NonsTacker <tackercg@appstate.edu>
2023-11-19 00:59:51 -0500

    DID I NOT COMMIT ANYTHING BEFORE?????

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,30 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public animal : string = 'Cat';
-  public count = 1;
+  animal: string = 'Cat';
+  count: number = 1;
   out1: string = ""; 
   out2: string = "";
   out3: string = "";
   out4: string = "";
   submitted: boolean = false;
 
-  onSubmit(form: NgForm) {
+  onSubmit() {
     this.submitted = true;
     if(this.animal.toLowerCase() === "cat") {
-      this.out1 = "\\    /\\";
-      this.out2 = "  )  (   ')";
-      this.out3 = " (  /    )";
-      this.out4 = "  \\(___)|";
-    } else if(this.animal.toLowerCase() === "mouse") {
-      this.out1 = "  __QQ"
-      this.out2 = " (_)_\">";
-      this.out3 = "_)";
-    } else if(this.animal.toLowerCase() === "bunny") {
       this.out1 = " ╱|、"
       this.out2 = "(˚ˎ 。7";
       this.out3 = "|、˜〵";
       this.out4 = "じしˍ,)ノ";
+    } else if(this.animal.toLowerCase() === "mouse") {
+      this.out1 = " _QQ";
+      this.out2 = "(_)_\">";
+      this.out3 = "(_";
+    } else if(this.animal.toLowerCase() === "bunny") {
+      this.out1 = "  oo__"
+      this.out2 = "<;___)------";
+      this.out3 = "  \" \"";
+      this.out4 = "";
     } else {
       this.out1 = "Fuk u";
     }
